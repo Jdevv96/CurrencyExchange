241f2b5ff72c3b090b6ac2e97eec0714915ae216
Josue Lopez-Tejada <omartejada@ymail.com>
2022-12-27 17:10:27 -0500

    Bootstrapping java app

---
+++ currencyExchange/src/main/java/com/personal_projects/currencyExchange/CurrencyExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.personal_projects.currencyExchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeApplication.class, args);
+	}
+
+}
