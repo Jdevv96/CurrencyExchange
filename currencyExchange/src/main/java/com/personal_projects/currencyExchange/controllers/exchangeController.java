31638274ec5fd45e2ade8281fbca983956fbf005
Josue_Tejada <omar14tejada@gmail.com>
2022-12-29 16:54:42 -0500

    Updating controller path

--- currencyExchange/src/main/java/com/personal_projects/currencyExchange/controllers/exchangeController.java
+++ currencyExchange/src/main/java/com/personal_projects/currencyExchange/controllers/exchangeController.java
@@ -1,4 +1,34 @@
 package com.personal_projects.currencyExchange.controllers;
 
+import org.jboss.logging.BasicLogger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Currency;
+
+@RestController
+@RequestMapping( path = "api/v1/currency")
 public class exchangeController {
+
+    // Main api path to get the latest rates
+    private static final String API_BASE_PATH = "https://api.exchangerate.host/latest";
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public Currency[] getLatestRates() {
+        Currency[] allRates = null;
+        try {
+            allRates = restTemplate.getForObject(API_BASE_PATH, Currency[].class);
+        } catch (ResourceAccessException e) {
+            // Catch netork I/O errors
+            System.out.println(e.getMessage());
+        } catch (RestClientResponseException e) {
+            // Catch status code errors
+            System.out.println(e.getRawStatusCode());
+        }
+        return allRates;
+    }
 }
--- currencyExchange/src/main/java/com/personal_projects/currencyExchange/model/currency.java
+++ currencyExchange/src/main/java/com/personal_projects/currencyExchange/model/currency.java
@@ -1,22 +1,32 @@
 package com.personal_projects.currencyExchange.model;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.HashMap;
 
+/*
+Currency model class that takes the base currency used in the exchange as well as date the info was accessed,
+with corresponding rates.
+ */
 public class currency {
 
     private String base;
     private LocalDate date;
+    //@NotNull(message = "Rate must not be blank")
     private HashMap<String, Float> rates = new HashMap<>();
 
+    // default base = EUR
     public currency() {
     }
 
+    // allows for getting rates by date
     public currency(LocalDate date, HashMap<String, Float> rates) {
         this.date = date;
         this.rates = rates;
     }
 
+    // allows for getting rates by date and by base
     public currency(String base, LocalDate date, HashMap<String, Float> rates) {
         this.base = base;
         this.date = date;
